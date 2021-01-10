91fba6b5a8772a0156d4df28e753eb29192dfa6f
Maxi <72995113+Maxi2601@users.noreply.github.com>
2021-01-10 18:55:13 -0300

    Sounds
    This is literally the

---
+++ sounds.js
@@ -0,0 +1,24 @@
+function Hubert() { // Hubert
+    var Hubert = new Audio("https://upload.wikimedia.org/wikipedia/commons/c/c3/Pl-Hubert.ogg")
+Hubert.play();
+}
+
+function the() { // The
+    var the = new Audio("https://upload.wikimedia.org/wikipedia/commons/5/5b/LL-Q1860_%28eng%29-Back_ache-The.wav")
+the.play()
+}
+
+function how() {
+    var how = new Audio("https://upload.wikimedia.org/wikipedia/commons/2/23/En-us-how.ogg")
+how.play()
+}
+
+function will() { //Will
+    var will = new Audio("https://upload.wikimedia.org/wikipedia/commons/6/67/En-us-will.ogg")
+will.play()
+}
+
+function Do() { // Do
+    var Do = new Audio("https://upload.wikimedia.org/wikipedia/commons/5/53/En-us-do-verb.ogg")
+Do.play()
+}
