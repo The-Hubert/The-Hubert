79fc51c9c9e20b65f4d22c7dfe4e9b5e7266101b
Maxi <72995113+Maxi2601@users.noreply.github.com>
2021-01-10 19:04:29 -0300

    Sounds
    the

--- sounds.js
+++ sounds.js
@@ -5,20 +5,20 @@ Hubert.play();
 
 function the() { // The
     var the = new Audio("https://upload.wikimedia.org/wikipedia/commons/5/5b/LL-Q1860_%28eng%29-Back_ache-The.wav")
-the.play()
+the.play();
 }
 
 function how() {
     var how = new Audio("https://upload.wikimedia.org/wikipedia/commons/2/23/En-us-how.ogg")
-how.play()
+how.play();
 }
 
 function will() { //Will
     var will = new Audio("https://upload.wikimedia.org/wikipedia/commons/6/67/En-us-will.ogg")
-will.play()
+will.play();
 }
 
 function Do() { // Do
     var Do = new Audio("https://upload.wikimedia.org/wikipedia/commons/5/53/En-us-do-verb.ogg")
-Do.play()
+Do.play();
 }
